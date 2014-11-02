528f033be685cc776a6a024f5553e23c4e4b966a
c0d3z3r0 <niewoehner.michael@me.com>
2014-11-02 18:39:06 +0100

    Changed click-dropdown to hover-dropdown

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
             <li><a href="#contact">Contact</a></li>
 
             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<span class="caret"></span></a>
+              <a href="" class="dropdown-toggle disableme" data-toggle="dropdown">Dropdown<span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
                 <li><a href="sites/test.php" target="#dyncontent">Action</a></li>
                 <li><a href="#">Another action</a></li>
--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+  /* dynamic content link handler */
   $("a").click(function(e){
     var target=$(this).attr('target');
     if(target.match(/^_.*/) == null){
@@ -14,10 +16,24 @@ $(document).ready(function(){
 
       $("li.active").toggleClass("active");
       $(this).parent().toggleClass("active");
-      if($(this).parent().parent().hasClass("dropdown-menu")) {
-        $(this).parent().parent().parent().toggleClass("active");
+        // dropdown hover close-on-click handler
+        $(".dropdown-menu").css("display","none");
       }
+    });
 
+  /** dropdown on hover **/
+  $('.dropdown').on({
+    mouseover: function(){
+      $(".dropdown-menu").css("display","block");
+    },
+    mouseleave: function(){
+      $(".dropdown-menu").css("display","none");
     }
   });
+  $('.dropdown-menu').on({
+    mouseleave: function(){
+      $(".dropdown-menu").css("display","none");
+    }
+  });
+
 });
