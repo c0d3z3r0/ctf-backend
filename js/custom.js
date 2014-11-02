b514263bfe76ea71cba7fff5d31285d62c77cb6f
c0d3z3r0 <niewoehner.michael@me.com>
2014-11-02 16:46:16 +0100

    Added active-class toggle to js dynamic links

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var target=$(this).attr('target');
     if(target.match(/^_.*/) == null){
       e.preventDefault();
+
       $.ajax({
         type: 'GET', 
         url: $(this).attr('href'),
@@ -10,6 +11,13 @@ $(document).ready(function(){
           $(target).html(data);
         }
       });
+
+      $("li.active").toggleClass("active");
+      $(this).parent().toggleClass("active");
+      if($(this).parent().parent().hasClass("dropdown-menu")) {
+        $(this).parent().parent().parent().toggleClass("active");
+      }
+
     }
   });
 });
