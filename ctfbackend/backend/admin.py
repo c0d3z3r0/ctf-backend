4ec8937324f0c4eda6db6443f588a3183b777e16
Michael Niewoehner <mniewoeh@stud.hs-offenburg.de>
2015-08-02 19:12:51 +0200

    Added Models to admin site

--- ctfbackend/backend/admin.py
+++ ctfbackend/backend/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Category, Challenge, Flag, Hint, Solve, BuyHint
 
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Challenge)
+class ChallengeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Flag)
+class FlagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Hint)
+class HintAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Solve)
+class SolveAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BuyHint)
+class BuyHintAdmin(admin.ModelAdmin):
+    pass
--- ctfbackend/backend/models.py
+++ ctfbackend/backend/models.py
@@ -8,6 +8,9 @@ from django.contrib.auth.models import User
 class Category(TimeStampedModel):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+    
 
 class Challenge(TimeStampedModel):
     name = models.CharField(max_length=50)
