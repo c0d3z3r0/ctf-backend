2501ab06e1f4bbb0e734f8f81968e67b87296a10
Michael Niewoehner <mniewoeh@stud.hs-offenburg.de>
2015-08-07 17:29:30 +0200

    Favicon - Changed type to png - Chrome favicon fix (didn't load)

--- ctfbackend/backend/urls.py
+++ ctfbackend/backend/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
+from django.http import HttpResponseRedirect
+from django.conf import settings
 from . import views
 
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'^favicon.png$', lambda x: HttpResponseRedirect(
+        settings.STATIC_URL + 'backend/favicon.png')),  # chrome favicon fix
 ]
