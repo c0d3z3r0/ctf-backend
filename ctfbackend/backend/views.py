d3fa27d19cc5f946ffa2c84e256f1a9a86f5b6e3
Michael Niewoehner <mniewoeh@stud.hs-offenburg.de>
2015-08-10 14:17:39 +0200

    Implemented SubmitView and adapted submit template

--- ctfbackend/backend/views.py
+++ ctfbackend/backend/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Flag, Solve, User
+
 # Create your views here.
 
 
 class HomeView(TemplateView):
     template_name = 'backend/base.html'
 
+
+class SubmitView(TemplateView):
+    template_name = 'backend/submit.html'
+
+    def post(self, request):
+        flag = request.POST.get('flag')
+        user = request.user
+
+        f = Flag.objects.filter(flag=flag).first()
+        if f:
+            if not f.user.filter(username=user.get_username()):
+                message = {
+                    'type': 'success',
+                    'glyph': 'ok',
+                    'text': 'Success!'
+                }
+                Solve(flag=f, user=user).save()
+            else:
+                message = {
+                    'type': 'info',
+                    'glyph': 'repeat',
+                    'text': 'Flag already submitted.'
+                }
+        else:
+            message = {
+                'type': 'danger',
+                'glyph': 'remove',
+                'text': 'Flag does not exist!'
+            }
+
+        context = {'message': message}
+        return render(request, self.template_name, context)
