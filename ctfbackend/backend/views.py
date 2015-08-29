16d296f06cedf5da9032270bbb28741ec9bcd400
Michael Niewoehner <mniewoeh@stud.hs-offenburg.de>
2015-08-29 22:49:18 +0200

    Implemented scoreboard

--- ctfbackend/backend/views.py
+++ ctfbackend/backend/views.py
@@ -40,3 +40,33 @@ class SubmitView(TemplateView):
 
         context = {'message': message}
         return render(request, self.template_name, context)
+
+
+class ScoreboardView(TemplateView):
+    template_name = 'backend/scores.html'
+
+    def chart(self):
+        xdata = ["Apple", "Apricot", "Avocado", "Banana", "Boysenberries", "Blueberries", "Dates", "Grapefruit", "Kiwi", "Lemon"]
+        ydata = [52, 48, 160, 94, 75, 71, 490, 82, 46, 17]
+        chartdata = {'x': xdata, 'y': ydata}
+        charttype = "lineChart"
+        chartcontainer = 'scoreboard'
+        data = {
+            'charttype': charttype,
+            'chartdata': chartdata,
+            'chartcontainer': chartcontainer,
+            'extra': {
+                'x_is_date': False,
+                'x_axis_format': '',
+                'tag_script_js': True,
+                'jquery_on_ready': False,
+            }
+        }
+        return data
+
+    def get(self, request):
+        scores = User.objects.annotate(
+            credits=Sum('solve__flag__credits')).order_by('credits').reverse()
+        context = {'scores': scores}
+        context.update(self.chart())
+        return render(request, self.template_name, context)
