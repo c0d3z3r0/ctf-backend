6862ce27a0550a072705ec153311dd310be98e6a
Michael Niewoehner <mniewoeh@stud.hs-offenburg.de>
2015-08-31 22:48:08 +0200

    templates: implement challenges template and correspoding filters

--- ctfbackend/backend/templatetags/backend_extras.py
+++ ctfbackend/backend/templatetags/backend_extras.py
@@ -9,3 +9,27 @@ def navactive(request, urls):
         return "active"
     return ""
 
+
+@register.filter
+def splitlist(l, n):
+    return list((l[i:i+n] for i in range(0, len(l), n)))
+
+
+@register.filter
+def catfilter(l, cat):
+    return [i for i in l if cat in i.categories.all()]
+
+
+@register.filter
+def isequal(i, comp):
+    return i == comp
+
+
+@register.filter
+def difficulty(i):
+    return ('success', 'info', 'warning', 'danger')[i-1]
+
+
+@register.filter
+def difficulty_pgbar(i):
+    return ('success', 'primary', 'warning', 'danger')[i-1]
