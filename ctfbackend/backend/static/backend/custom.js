7775f5056363c877b4fb461930c54f3d1051d426
Michael Niewoehner <mniewoeh@stud.hs-offenburg.de>
2015-08-31 17:43:18 +0200

    Adjust dropdown alignment according to panel position

--- ctfbackend/backend/static/backend/custom.js
+++ ctfbackend/backend/static/backend/custom.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 
+    /* Adjust dropdown alignment according to panel position */
+    $('.dropdown-toggle').on("click", function() {
+        var dropdown = $(this).parent().children('.dropdown-menu'),
+            offsetLeft = $(this).offset().left,
+            dropdownWidth = dropdown.width(),
+            docWidth = $(window).width();
+
+        if (!(offsetLeft + dropdownWidth <= docWidth))
+            dropdown.addClass('pull-right');
+    });
+
 });
