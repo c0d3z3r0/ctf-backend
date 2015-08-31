22a31384ce4f1000e7cb49ea4aaafa099abe4a8e
Michael Niewoehner <mniewoeh@stud.hs-offenburg.de>
2015-08-31 22:49:39 +0200

    views: scoreboard - remove chart test code - subtract bought hints from earned credits

--- ctfbackend/backend/views.py
+++ ctfbackend/backend/views.py
@@ -49,30 +49,11 @@ class SubmitView(TemplateView):
 class ScoreboardView(TemplateView):
     template_name = 'backend/scores.html'
 
-    def chart(self):
-        xdata = ["Apple", "Apricot", "Avocado", "Banana", "Boysenberries", "Blueberries", "Dates", "Grapefruit", "Kiwi", "Lemon"]
-        ydata = [52, 48, 160, 94, 75, 71, 490, 82, 46, 17]
-        chartdata = {'x': xdata, 'y': ydata}
-        charttype = "lineChart"
-        chartcontainer = 'scoreboard'
-        data = {
-            'charttype': charttype,
-            'chartdata': chartdata,
-            'chartcontainer': chartcontainer,
-            'extra': {
-                'x_is_date': False,
-                'x_axis_format': '',
-                'tag_script_js': True,
-                'jquery_on_ready': False,
-            }
-        }
-        return data
-
     def get(self, request):
         scores = User.objects.annotate(
-            credits=Sum('solve__flag__credits')).order_by('credits').reverse()
+            credits=Sum('solve__flag__credits')-Coalesce(Sum('buyhint__hint__price'), 0)
+        ).order_by('credits').reverse()
         context = {'scores': scores}
-        context.update(self.chart())
         return render(request, self.template_name, context)
 
 
