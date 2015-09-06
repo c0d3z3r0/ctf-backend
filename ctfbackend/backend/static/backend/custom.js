a81c4f65c3df4cf6c2d6ed6d30bc2e63d32304d0
Michael Niewoehner <mniewoeh@stud.hs-offenburg.de>
2015-09-06 19:52:47 +0200

    js: dropdowns - use dropdown-menu-right for dropdown alignment - adjust open dropdowns (needed later for buying hints) - Prevent dropdown close on click to allow copy of text - make code more generic

--- ctfbackend/backend/static/backend/custom.js
+++ ctfbackend/backend/static/backend/custom.js
@@ -1,24 +1,37 @@
 $(document).ready(function(){
 
     /* Adjust dropdown alignment according to panel position */
-    $('.dropdown-toggle').on("click", function() {
-        var dropdown = $(this).parent().children('.dropdown-menu'),
-            offsetLeft = $(this).offset().left,
+    function adjustDDAlign(el) {
+        var dropdown = el.parent().children('.dropdown-menu'),
+            offsetLeft = el.offset().left,
             dropdownWidth = dropdown.width(),
             docWidth = $(window).width();
 
         if (!(offsetLeft + dropdownWidth <= docWidth))
-            dropdown.addClass('pull-right');
+            dropdown.addClass('dropdown-menu-right');
+    }
+    $('.dropdown-toggle').on("click", function() {
+        adjustDDAlign($(this));
+    }).each(function() {
+        adjustDDAlign($(this));
     });
 
-    /* Prevent challenge dropdown close on click to allow copy of text */
-    $('.btn-group-chal .dropdown-menu').on("click", function(e) {
+    /* Prevent dropdown close on click to allow copy of text */
+    $('.dropdown-menu').on("click", function(e) {
+        $('.dropdown-menu').not(
+            $(this)).not($('.dropdown-menu .btn-group').closest('.dropdown-menu')
+        ).parent().removeClass('open');
         e.stopPropagation();
     });
 
     /* Allow multi-level dropdowns */
-    $('div.dropdown-menu .dropdown-toggle').on('click', function() {
+    $('.dropdown-menu .dropdown-toggle').on('click', function(e) {
+        if(!$(this).parent().hasClass('open'))
+            $('.dropdown-menu').not(
+                $('.dropdown-menu .btn-group').closest('.dropdown-menu')
+            ).parent().removeClass('open');
         $(this).parent().toggleClass('open');
+        e.stopPropagation(); // prevent close caused by button click event on parent
     });
 
 });
