43a90a8a82eabf67b0090bf17113ed4642c23f9a
Michael Niewoehner <mniewoeh@stud.hs-offenburg.de>
2015-10-25 19:26:32 +0100

    dynprefs: remove ctf@hso specific texts

--- ctfbackend/backend/dynprefs.py
+++ ctfbackend/backend/dynprefs.py
@@ -8,7 +8,7 @@ class BackendTitle(StringPreference):
     section = 'general'
     name = 'sitename'
     verbose_name = 'Backend Title'
-    default = 'CTF@HSO'
+    default = 'CTF-Backend'
     help_text = 'The website title (Default: '+default+')'
 
 
@@ -414,7 +414,7 @@ class HomeTitle(StringPreference):
     section = 'home'
     name = 'title'
     verbose_name = 'Home Title'
-    default = 'Welcome to CTF@HSO ...'
+    default = 'Welcome to the CTF-Backend.'
     help_text = 'Home site title (Default: '+default+')'
 
 
@@ -423,9 +423,8 @@ class HomeLead(LongStringPreference):
     section = 'home'
     name = 'lead'
     verbose_name = 'Home Lead'
-    default = '... the Capture the Flag Workshop of the<br>University of ' \
-              'Applied Sciences in Offenburg.'
-    help_text = 'Home site lead text (Default: '+default+')'
+    default = ''
+    help_text = 'Home site lead text'
 
 
 @global_preferences_registry.register
